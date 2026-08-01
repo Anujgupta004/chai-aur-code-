95dc06333cef025fedf95bc054748f53d338b654
Anuj Gupta <anujg9637@gmail.com>
2026-08-01 10:04:14 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "hello@gmail.com"
+var accountPassword = "1234567890"
+let accountState;
+accountCity = "mumbai"
+accountEmail = "hell@gmail.com"
+accountPassword = "jay"
+accountCity = "kanpur"
+// accountId = 54321
+// console.log(accountId);
+console.table([accountEmail , accountPassword , accountCity , accountId , accountState])
+// only const and let ka use karenge var ka nhi 
