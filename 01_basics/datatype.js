07788551b1559e35f6af22dc9c9694c07c1bdfdc
Anuj Gupta <anujg9637@gmail.com>
2026-08-01 10:46:19 +0000

    datatype done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,7 @@
+let name = "Anuj"
+let age = 18
+let IsLoggedIn = false
+// console.log(age)
+console.table([name , age, IsLoggedIn])
+console.log(typeof null);// object
+console.log(typeof null); // indefined
