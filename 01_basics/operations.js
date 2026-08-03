c07a8536a2ab8dcd78cf1f790765eb8c39763b5f
Anuj Gupta <anujg9637@gmail.com>
2026-08-03 05:54:41 +0000

    commit

---
+++ 01_basics/operations.js
@@ -0,0 +1,26 @@
+let num1 = 5
+let num2 = 4
+let num3 = 6
+let num5 = num1 + num2 + num3
+console.log(num5)
+console.log( "1" + 2 + 5) // js me 1st me string then sabhi me string leta hai 
+console.log( 1 + 12 + "3") // js me 1 me non string hoga to acrd to datatype pe kaam karega 
+console.log((1 + 2) * 2)
+console.log(2 + "")
+console.log(null)
+console.log(typeof (null)) 
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter)
+
+let x = 5
+let y = x++
+console.log(y)
+console.log(x)
+let m = 2
+let n = ++m
+console.log(n)
+console.log(m)
+
+
