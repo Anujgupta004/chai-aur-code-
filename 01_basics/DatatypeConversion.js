6898be607c0007e447302d72c9a2a35688ceb868
Anuj Gupta <anujg9637@gmail.com>
2026-08-03 04:44:14 +0000

    commited

---
+++ 01_basics/DatatypeConversion.js
@@ -0,0 +1,17 @@
+// let score = "34w"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+// let IsNumber = Number(score)
+// console.log(IsNumber)
+// console.log(typeof (IsNumber))
+// let IsLoggedIn = 1
+// let IsLoggedIn = 0
+// let IsLoggedIn = 1
+// let BolleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(BolleanIsLoggedIn)
+ let IsNumber = 34
+ let stringIsNumber = String(IsNumber)
+ console.log(stringIsNumber)
+ console.log(typeof(stringIsNumber))
