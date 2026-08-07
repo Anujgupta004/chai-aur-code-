24b13b2f0e075551482d00210ebd8e0bad2e35b5
Anuj Gupta <anujg9637@gmail.com>
2026-08-07 16:24:43 +0530

    Add stackheap.js to illustrate variable and object handling
    Demonstrate variable assignment and object reference behavior.

---
+++ 01_basics/stackheap.js
@@ -0,0 +1,15 @@
+let name = "Anuj"
+let otherName = name
+otherName = "Amit"
+console.log(otherName)
+console.log(name)
+
+
+let userOne = {
+  email : "ram@gmail.com",
+  upi : "upi@pay" 
+}
+let userTwo = userOne 
+userTwo.email = "user@gmail.com"
+console.log(userTwo)
+console.log(userOne)
