58d71692aaa9a9f1d2170968b723ce9076ed0862
Anuj Gupta <anujg9637@gmail.com>
2026-08-08 07:04:26 +0000

    All type of string commit

---
+++ 01_basics/string.js
@@ -0,0 +1,39 @@
+const name = "Anuj"
+const age = 23
+console.table(typeof name)
+console.log(`My name is ${name} and Iam ${age} years old`)
+const gameName = new String("Cricket")
+console.log(gameName)
+// console.log(gameName.length)
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.endsWith("r"))
+// console.log(gameName.startsWith("c"))
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("i"))
+const newstring = gameName.substring(3,5)
+console.log(newstring)
+console.log(typeof gameName)
+
+// const a = "10"
+// const b = "12"
+// if (a<b) {
+// console.log(`${a} is less than ${b}`)
+// }
+// else if (b<a)  {
+//     console.log(`${b} is less than ${a} `)
+// }
+// else {
+//     console.log(`${a} is equal to ${b}`)
+// }
+// const scorCard = "   nahi pta"
+// console.log(scorCard)
+// console.log(scorCard.length)
+// console.log(scorCard.trim())
+// console.log(scorCard.trim().length)
+const str1 = "Anuj"
+const str2 = "kumar"
+console.log(str1.concat(""), str2)
+const mood = "Happy! "
+console.log(`I feel ${mood.repeat(5)}`)
+
