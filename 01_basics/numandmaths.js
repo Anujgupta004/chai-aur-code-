cff6f06e6c59bf0f98877076d26a60d190673a38
Anuj Gupta <anujg9637@gmail.com>
2026-08-08 08:46:04 +0000

    comit

---
+++ 01_basics/numandmaths.js
@@ -0,0 +1,21 @@
+const score = 200
+console.log(score)
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toFixed(2))
+const data = 12.365
+console.log(data.toPrecision(3))
+console.log(Math)
+console.log(Math.abs(-1))
+console.log(Math.round(23.32))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(23.3))
+console.log(Math.min(2,32,32,1))
+console.log(Math.max(2,32,32,1))
+console.log(Math.random())
+const num = Math.floor((Math.random() * 10 )+ 1)
+console.log(num)
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
