aa41cb82a4f284962b5d0467e7f6a2a47faf0ff2
Anuj Gupta <anujg9637@gmail.com>
2026-08-10 05:11:39 +0000

    nk

---
+++ 01_basics/date.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+
+let myCreatedDate = new Date("08-10-2026")
+console.log(myCreatedDate.toDateString())
