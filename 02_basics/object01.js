b4cd223b20967b399198a9a5d9994e25da9f4c10
Anuj Gupta <anujg9637@gmail.com>
2026-08-23 08:52:45 +0000

    done

---
+++ 02_basics/object01.js
@@ -0,0 +1,25 @@
+const tinderUser = {}
+tinderUser.Id = "123abc",
+tinderUser.name = "Anuj"
+console.log(Object.keys(tinderUser))
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+// console.log(tinderUser)
+// console.log(tinderUser["name"])
+// const regularUser = {
+//     email :
+//      "Email",
+//     newUser : {
+//         name : "Anuj",
+//         age : 23
+//     }
+// }
+// console.log(regularUser)
+// const obj1 = {
+//     1:"a",2:"b"
+// }
+// const obj2 = {
+//     3 : "c",
+//     4 : "d"
+// }
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3)
