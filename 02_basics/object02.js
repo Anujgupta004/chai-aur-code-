d6839d9075a63bc037e2d002d1da01e43f5cc49f
Anuj Gupta <anujg9637@gmail.com>
2026-08-25 07:59:53 +0000

    done

---
+++ 02_basics/object02.js
@@ -0,0 +1,32 @@
+// Constructor function 
+// function Person (fname , lname , contact)  {
+//     this.fname = fname,
+//     this.lname = lname,
+//     this.contact = contact,
+//     this.getFullName = function(){
+//         console.log(this.fname , this.lname)
+//     }
+    
+// }
+// const person1 = new Person("Anuj","Gupta","9336")
+// console.log(person1)
+// const person2 = new Person("raju", "pandit","3234")
+// console.log(person2)
+// console.log(person1.getFullName())
+
+
+// Es6 ke baad
+class Person {
+    constructor(Fname , Lname , Contact){
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.Contact = Contact;
+    }
+    getFullName() {
+        console.log(this.Fname , this.Lname)
+
+    }
+
+}
+const person1 = new Person("Anuj","Gupta","9336")
+console.log(person1)
