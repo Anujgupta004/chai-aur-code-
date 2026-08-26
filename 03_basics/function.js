d91174d2f123db72831d3d9dd2d9f26499d06a71
Anuj Gupta <anujg9637@gmail.com>
2026-08-26 05:05:15 +0000

    done

---
+++ 03_basics/function.js
@@ -0,0 +1,26 @@
+function myName(){
+    console.log("A")
+    console.log("N")
+    console.log("U")
+    console.log("J")
+}
+myName()
+
+function addTwoNumber(a,b) {
+console.log(a+b)
+}
+addTwoNumber(2,3)
+
+function addTwoNumber(a,b){
+    let sum = a+b
+    return sum
+    console.log("ram")
+}
+const sum = addTwoNumber(2,2)
+console.log(`the sum is : ${sum}`)
+
+
+function loggedIn(username) {
+    return `${username} is just loggedIn`
+}
+console.log(loggedIn("ram"))
