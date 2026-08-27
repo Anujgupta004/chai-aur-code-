1cef4e4733637cdc036c6a3cb089e21dce436804
Anuj Gupta <anujg9637@gmail.com>
2026-08-27 06:49:27 +0000

    h

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -6,19 +6,34 @@ var c = 3
 
 // console.log(a)
 // console.log(b)
-console.log(c)
+// console.log(c)
 
 
-function one(){
-    const username = "Hitesh";
-    function two(){
-        const website = "youtube"
-        // console.log(username)
-        console.log(website)
-    }
-    // console.log(website)
-    console.log(username)
+// function one(){
+//     const username = "Hitesh";
+//     function two(){
+//         const website = "youtube"
+//         // console.log(username)
+//         console.log(website)
+//     }
+//     // console.log(website)
+//     console.log(username)
 
-    // two()
+    
+
+//     // two()
+// }
+// one()
+
+
+/// ++++++++++++++++++++MOST IMPORTANT +++++++++++++++//////
+
+function addOne(num){
+    return num+1
 }
-one()
+addOne(5)
+
+const addThree = function(num){
+    return num+3
+}
+console.log(addThree(5)) 
