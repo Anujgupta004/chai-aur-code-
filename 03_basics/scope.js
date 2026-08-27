f00982bfee6ed19d5ca5d9903048df87b1fa2610
Anuj Gupta <anujg9637@gmail.com>
2026-08-27 06:15:32 +0000

    g

---
+++ 03_basics/scope.js
@@ -0,0 +1,24 @@
+if (true){
+let a = 1
+const b = 2
+var c = 3
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
+
+
+function one(){
+    const username = "Hitesh";
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+        console.log(website)
+    }
+    // console.log(website)
+    console.log(username)
+
+    // two()
+}
+one()
